1a8bf093555c9c710b42773f6d1dee3707e8565b
sainiashu <saini.ashu90@gmail.com>
2019-01-16 20:38:15 +0530

    add http calls with fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req , res ){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  //hard coding for simplicity. Pretend it hits the real database.
+  res.json([
+    {"id":1,"firstName": "Bob", "lastName":"Smith", "email": "bob@gmail.com"},
+    {"id":2,"firstName": "John", "lastName":"Smith", "email": "john@gmail.com"},
+    {"id":3,"firstName": "keth", "lastName":"Tyalor", "email": "keth@gmail.com"},
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import './index.css';
-import numeral from 'numeral';
 
-const cousreValue = numeral(1000).format('$0,0.00'); //eslint-disable-line no-console
-console.log(`I would pay ${courseValue},for this course!`);
+import {getUsers} from './api/userApi';
+//import { get } from 'http';
+
+//Populate table of users via api call
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody+= `<tr>
+    <td><a href="#" data-t-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`
+
+  });
+
+  global.document.getElmentById('users').innerHTML = usersBody;
+});
+// import numeral from 'numeral';
+
+// const cousreValue = numeral(1000).format('$0,0.00'); //eslint-disable-line no-console
+// console.log(`I would pay ${courseValue},for this course!`);
